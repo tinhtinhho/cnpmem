6ea332e2af04dcfbbb2145fe56aef7352646130a
unknown <vytu2194@gmail.com>
2018-04-19 12:15:52 +0700

    first commit

---
+++ KHO/Controllers/KhoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KHO.Models;
+
+namespace KHO.Controllers
+{
+    public class KhoController : Controller
+    {
+        DbQLKhoDataContext db = new DbQLKhoDataContext();
+        private List<MatHang> LayMatHang(int count)
+        {
+            return db.MatHangs.OrderByDescending(a => a.MaMH).Take(count).ToList();
+        }
+        // GET: Kho
+        public ActionResult Index()
+        {
+            var mathang = LayMatHang(4);
+            return View(mathang);
+        }
+    }
+}
